0c695138a0c9151e991b8bad78e01cb13b59918a
Karthik Urs <ursk@umich.edu>
2021-03-04 16:32:21 -0500

    got moteus control thru python working. calibrations are restorable, which is sweet

--- moteus_testing/moteus_can_test.py
+++ moteus_testing/moteus_can_test.py
@@ -3,16 +3,38 @@
 import asyncio
 import math
 import moteus
+import moteus.moteus_tool as mt
+import argparse
+import os
 
 async def main():
     c = moteus.Controller()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--calibrate", action='store_true')
+    # parser.add_argument(
+    #     '-t', '--target', type=str, action='append', default=[],
+    #     help='destination address(es) (default: autodiscover)')
+    # args = parser.parse_args(['--target','1','--calibrate'])
+    # mtr = mt.Runner(args)
+    # await mtr.start()
+    # os.system("python3 -m moteus.moteus_tool --target 1 --calibrate")
+    os.system("python3 -m moteus.moteus_tool --target 1 --restore-cal moteus_logs/ri50_cal.log")
     await c.set_stop()  # in case there was a fault
-
+    i = 1
     while True:
-        print(await c.set_position(position=math.nan, query=True))
+        # print(await c.set_position(position=math.nan, velocity=float(i%2), query=True))
+        pos = 2.0*i
+        # print(await c.set_position(position=pos,\
+        #     kp_scale = 2, kd_scale = 0.5,\
+        #     watchdog_timeout=math.nan, query=True))
+        print(await c.set_position(position=math.nan, velocity=pos,\
+            kp_scale = 2, kd_scale = 0.5,\
+            watchdog_timeout=math.nan, query=True))
+        print(pos)
         # print(await c.query())
         # print(c.id)
         # return
-        await asyncio.sleep(0.02)
+        await asyncio.sleep(2.0)
+        i = -i
 
 asyncio.run(main())
