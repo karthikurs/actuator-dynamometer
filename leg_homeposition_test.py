fe0ccabf3acbbf6be17f0b750e29c9b8f4606a39
ChallenEnninful <Challoenenninful@gmail.com>
2021-06-05 16:46:07 -0400

    Home pose script implemented -Challen

--- leg_homeposition_test.py
+++ leg_homeposition_test.py
@@ -66,29 +66,41 @@ async def main():
             t1 = dt[0,0]
             t2 = dt[1,0]
             # print('fx={} fy={} t1={} t2={}'.format(fx, fy, t1, t2))
-            replies = await transport.cycle(\
-                            [c1.make_stop(query=True),\
-                            c2.make_stop(query=True)])
+            # replies = await transport.cycle(\
+            #                 [c1.make_stop(query=True),\
+            #                 c2.make_stop(query=True)])
 
 
-            reply1 = replies[0]
-            reply2 = replies[1]
-            p1, v1, t1 = parse_reply(reply1, 6)
-            p2, v2, t2 = parse_reply(reply2, 6)
+            # reply1 = replies[0]
+            # reply2 = replies[1]
+            # p1, v1, t1 = parse_reply(reply1, 6)
+            # p2, v2, t2 = parse_reply(reply2, 6)
 
 
             # p1, v1, t1 = parse_reply(await c1.set_current(q_A=t1/kt_1, d_A=0.0, query=True), g=6)
             # p2, v2, t2 = parse_reply(await c2.set_current(q_A=t2/kt_2, d_A=0.0, query=True), g=6)
-            print('p1={},v1={},t1={}'.format(p1, v1, t1))
+            # print('p1={},v1={},t1={}'.format(p1, v1, t1))
+            # print('p2={},v2={},t2={}'.format(p2, v2, t2))
+
+
+        
+            adj_1=0.16087623512950663
+            adj_2=1.0025842766155904
+            d_p1=0+adj_1
+            d_p2=0+adj_2
+            d_pos1=(d_p1*6)/(2*np.pi)
+            d_pos2=(d_p2*6)/(2*np.pi)
+
+
+            p1, v1, t1 = parse_reply(await c1.set_position(position=np.NaN, velocity=0.5, stop_position=d_pos1,query=True), g=6)
+            p2, v2, t2 = parse_reply(await c2.set_position(position=np.NaN, velocity=0.5, stop_position=d_pos2,query=True), g=6)
+
+            p1_h=p1-adj_1
+            p2_h=p2-adj_2
+
+            # print('p1_h={},v1={},t1={}'.format(p1_h, v1, t1))
             print('p2={},v2={},t2={}'.format(p2, v2, t2))
-
-            p1_h=p1-0.16087623512950663
-            p2_h=p2-1.0025842766155904
-
-            print('p1_h={},v1={},t1={}'.format(p1_h, v1, t1))
             print('p2_h={},v2={},t2={}'.format(p2_h, v2, t2))
-
-
  
             #Zero out tests
             #Home pose defaults
